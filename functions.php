d423f9f1cc2e6b6d3f9db3b582855a479e80e472
Sam Sarjudeen <Sam Sarjudeen>
2024-06-22 22:15:15 +0100

    replaced customiser hero section with home ACF

--- functions.php
+++ functions.php
@@ -20,6 +20,20 @@ function kcg_enqueue_scripts()
     wp_enqueue_script('gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.0/gsap.min.js', array(), '3.11.0', true);
     wp_enqueue_script('lottie', 'https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.6/lottie.min.js', array(), null, true);
 
+    // Enqueue Customised Tagling JS
+    wp_enqueue_script('custom-tagline-js', get_template_directory_uri() . '/assets/js/hero-tagline.js', array(), null, true);
+
+    // Enqueue Customised Elvanto CSS & JS
+    wp_enqueue_style('custom-elvanto-css', get_template_directory_uri() . '/assets/css/elvanto-swiper.css', array(), null);
+    wp_enqueue_script('custom-elvanto-js', get_template_directory_uri() . '/assets/js/elvanto-swiper.js', array(), null, true);
+
+    wp_enqueue_script( 'customizer-drag-drop', get_template_directory_uri() . '/assets/js/customizer-drag-drop.js', array( 'jquery', 'jquery-ui-sortable' ), '', true );
+
+    // Localize script with PHP variables
+    $theme_vars = array(
+        'themeDirectory' => get_template_directory_uri(),
+    );
+    wp_localize_script('custom-js', 'themeVars', $theme_vars);
 
 }
 add_action('wp_enqueue_scripts', 'kcg_enqueue_scripts');
@@ -32,6 +46,7 @@ function kcg_menus()
             'left-menu' => __('Left Menu', 'kcg'),
             'right-menu' => __('Right Menu', 'kcg'),
             'hero-buttons' => __('Hero Buttons Menu', 'kcg'),
+            'footer_menu' => __('Footer Menu', 'theme'),
         )
     );
 }
@@ -40,74 +55,30 @@ add_action('init', 'kcg_menus');
 
 function kcg_customize_register($wp_customize)
 {
+    // Add section for footer
+    $wp_customize->add_section('footer', array(
+        'title' => __('Footer', 'kcg'),
+        'priority' => 30,
+    ));
 
-    // Add section for hero content
-    $wp_customize->add_section(
-        'hero_section',
-        array(
-            'title' => __('Hero Section', 'kcg'),
-            'priority' => 30,
-        )
-    );
+    // Church logo setting
+    $wp_customize->add_setting('church_logo');
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'church_logo', array(
+        'label' => __('Church Logo', 'kcg'),
+        'section' => 'footer',
+        'settings' => 'church_logo',
+    )));
 
-    // Add setting and control for hero background video
-    $wp_customize->add_setting(
-        'hero_background_video',
-        array(
-            'default' => '',
-            'sanitize_callback' => 'esc_url_raw',
-        )
-    );
-
-    $wp_customize->add_control(
-        new WP_Customize_Upload_Control(
-            $wp_customize,
-            'hero_background_video',
-            array(
-                'label' => __('Hero Background Video', 'kcg'),
-                'section' => 'hero_section',
-                'settings' => 'hero_background_video',
-            )
-        )
-    );
-
-    // Add setting and control for hero title
-    $wp_customize->add_setting(
-        'hero_title',
-        array(
-            'default' => __('Default Title', 'kcg'),
-            'sanitize_callback' => 'sanitize_text_field',
-        )
-    );
-
-    $wp_customize->add_control(
-        'hero_title',
-        array(
-            'label' => __('Hero Title', 'kcg'),
-            'section' => 'hero_section',
-            'type' => 'text',
-        )
-    );
-
-    // Add setting and control for hero subtitle
-    $wp_customize->add_setting(
-        'hero_subtitle',
-        array(
-            'default' => __('Default Subtitle', 'kcg'),
-            'sanitize_callback' => 'sanitize_text_field',
-        )
-    );
-
-    $wp_customize->add_control(
-        'hero_subtitle',
-        array(
-            'label' => __('Hero Subtitle', 'kcg'),
-            'section' => 'hero_section',
-            'type' => 'text',
-        )
-    );
+    // Affiliation logo setting
+    $wp_customize->add_setting('affiliation_logo');
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'affiliation_logo', array(
+        'label' => __('Affiliation Logo', 'kcg'),
+        'section' => 'footer',
+        'settings' => 'affiliation_logo',
+    )));
 }
 add_action('customize_register', 'kcg_customize_register');
+
 
 function kcg_setup()
 {
@@ -132,7 +103,8 @@ function kcg_setup()
             'slug' => 'dark',
             'color' => '#333333',
         ),
-    ));
+    )
+    );
 }
 add_action('after_setup_theme', 'kcg_setup');
 
@@ -155,14 +127,5 @@ function kcg_enqueue_block_editor_assets()
     );
 }
 add_action('enqueue_block_editor_assets', 'kcg_enqueue_block_editor_assets');
-
-
-// function enqueue_swiper_scripts()
-// {
-//     wp_enqueue_style('swiper-css', 'https://unpkg.com/swiper/swiper-bundle.min.css');
-//     wp_enqueue_script('swiper-js', 'https://unpkg.com/swiper/swiper-bundle.min.js', array(), null, true);
-//     wp_enqueue_script('custom-swiper-init', get_template_directory_uri() . '/assets/js/custom-swiper.js', array('swiper-js'), null, true);
-// }
-// add_action('wp_enqueue_scripts', 'enqueue_swiper_scripts');
 
 ?>

059f47684853cdefa7b8de4c14dc2cb88ddab6b1
Sam Sarjudeen <Sam Sarjudeen>
2024-06-23 22:07:14 +0100

    proper title

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php the_title(); ?> | <?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
 

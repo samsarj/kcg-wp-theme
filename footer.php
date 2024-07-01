06e88afd698d4d4f9e3336dd22a36cc255d4fefc
Sam Sarjudeen <samsarj@gmail.com>
2024-07-01 20:20:56 +0000

    added logo links

--- footer.php
+++ footer.php
@@ -13,15 +13,20 @@
         </div>
         <div class="logos">
             <?php if (get_theme_mod('church_logo')): ?>
+                <a href="<?php echo esc_url(get_theme_mod('church_logo_link')); ?>">
                 <img src="<?php echo esc_url(get_theme_mod('church_logo')); ?>" alt="Church Logo">
+            </a>
             <?php endif; ?>
             <?php if (get_theme_mod('affiliation_logo')): ?>
+                <a href="<?php echo esc_url(get_theme_mod('affiliation_logo_link')); ?>">
                 <img src="<?php echo esc_url(get_theme_mod('affiliation_logo')); ?>" alt="Affiliation Logo">
+            </a>
             <?php endif; ?>
         </div>
     </div>
     <div class="copyright">
-        <p>&copy; <?php echo date('Y'); ?> King's Church Guildford. All rights reserved.</p>
+        &copy; <?php echo date('Y'); ?>. All rights reserved.<br>
+        King's Church Guildford is a Charitable Incorporated Organisation (CIO) registered in England & Wales - Number 1158254
     </div>
     <?php wp_footer(); ?>
 </footer>

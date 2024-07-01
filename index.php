30abf167e45bba7eaf4ae2cb3466705d7f287217
Sam Sarjudeen <samsarj@gmail.com>
2024-07-01 20:21:37 +0000

    matches page

--- index.php
+++ index.php
@@ -1,26 +1,15 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title('|', true, 'right'); ?></title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-    <?php get_header(); ?>
-    <!-- Main content -->
-    <div id="content">
+<?php
+get_header(); ?>
+
+<section class="page-container">
+    <div class="page-content">
         <?php
-        if ( have_posts() ) :
-            while ( have_posts() ) : the_post();
-                the_content();
-            endwhile;
-        endif;
+        while (have_posts()):
+            the_post();
+            the_content();
+        endwhile;
         ?>
     </div>
-    <?php get_footer(); ?>
-    <script src="<?php echo get_template_directory_uri(); ?>/script.js"></script>
-    <?php wp_footer(); ?>
-</body>
-</html>
+</section>
+
+<?php get_footer(); ?>

1f55bb286d8235856af0d1b974cd8d341f0d52d0
samsarj <samsarj@gmail.com>
2025-07-14 21:14:25 +0100

    pruning

--- assets/js/hero-tagline.js
+++ assets/js/hero-tagline.js
@@ -5,47 +5,6 @@ const texts = [
 ];
 
 let currentSet = 0;
-<<<<<<< HEAD
-const line1 = document.getElementById("line1");
-const line2 = document.getElementById("line2");
-const line3 = document.getElementById("line3");
-
-function animateText(line, newText) {
-  gsap.to(line, {
-    opacity: 0,
-    scale: 0.5,
-    rotationX: 180,
-    duration: 0.5,
-    onComplete: () => {
-      line.textContent = newText;
-      gsap.to(line, {
-        opacity: 1,
-        scale: 1,
-        rotationX: 0,
-        duration: 0.5,
-      });
-    },
-  });
-}
-
-function updateText() {
-  const nextSet = (currentSet + 1) % texts.length;
-
-  if (texts[currentSet][0] !== texts[nextSet][0]) {
-    animateText(line1, texts[nextSet][0]);
-  }
-  if (texts[currentSet][1] !== texts[nextSet][1]) {
-    animateText(line2, texts[nextSet][1]);
-  }
-  if (texts[currentSet][2] !== texts[nextSet][2]) {
-    animateText(line3, texts[nextSet][2]);
-  }
-
-  currentSet = nextSet;
-}
-
-setInterval(updateText, 3000);
-=======
 
 function initTaglineAnimation() {
   const taglineDiv = document.querySelector('.tagline');
@@ -115,4 +74,3 @@ document.addEventListener('DOMContentLoaded', function() {
     console.error('GSAP library not loaded. Animation will not work.');
   }
 });
->>>>>>> 3beaf08 (Starting afresh)
